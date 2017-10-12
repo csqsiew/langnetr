9df687f8408bfe710e97a37a516b9aa254027eaf
Cynthia Siew <cynsiewsq@gmail.com>
2017-10-12 13:16:51 +0100

    added a new function, toedgelist, and updated vignette

---
+++ R/toedgelist.R
@@ -0,0 +1,17 @@
+#' Converts a list of words to an igraph network object.
+#'
+#' @param langnet An igraph object created using the \code{tolangnet} function and with label names created with the \code{nodeindex} function.
+#' @return A dataframe of the *labeled* edgelist of the language network created from \code{wordlist}.
+#' @examples
+#' somewords <- c('cat', 'bat', 'cap', 'cape')
+#' somewordsnet <- tolangnet(somewords)
+#' somewordsnet <- nodeindex(somewordsnet, somewords)
+#' somewords.edgelist <- toedgelist(somewordsnet)
+#' head(somewords.edgelist)
+
+toedgelist <- function(langnet) {
+  el <- igraph::as_edgelist(langnet)
+  el.names <- data.frame(word1 = igraph::V(langnet)[el[,1]]$name, #replace the node id with its name
+                         word2 = igraph::V(langnet)[el[,2]]$name)
+  return(el.names)
+}
